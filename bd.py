419aa6427f5faf475b562fd5a8b58d6e4116c2aa
miserior <51886505+miserior@users.noreply.github.com>
2024-09-24 22:47:34 -0500

    Update bd.py

--- bd.py
+++ bd.py
@@ -1,19 +1,21 @@
-import os
-from dotenv import load_dotenv
 import pyodbc
 
-load_dotenv() 
+def conectar_sql_server(direccion_servidor, nombre_bd, nombre_usuario, password):
+    """
+    Función para establecer una conexión a una base de datos SQL Server.
 
-direccion_servidor = os.getenv('direccion_servidor')
-nombre_bd = os.getenv('nombre_bd')
-nombre_usuario = os.getenv('nombre_usuario')
-password = os.getenv('password')
+    Args:
+        direccion_servidor (str): Dirección del servidor SQL Server (e.g., 'CC102-16\SA').
+        nombre_bd (str): Nombre de la base de datos a la que se conectará.
+        nombre_usuario (str): Nombre de usuario para la autenticación.
+        password (str): Contraseña para la autenticación.
 
-try:
-    conexion = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + 
-                              direccion_servidor+';DATABASE='+nombre_bd+';UID='+nombre_usuario+';PWD=' + password)
-    print("\n"*2)
-    print("conexión exitosa")
-    
-except Exception as e:
-    print("Ocurrió un error al conectar a SQL Server: ", e)
+    Returns:
+        pyodbc.Connection: Objeto de conexión si la conexión es exitosa, None en caso contrario.
+    """
+    try:
+        conexion = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + 
+                                  direccion_servidor+';DATABASE='+nombre_bd+';UID='+nombre_usuario+';PWD=' + password)
+        return conexion
+    except Exception as e:
+        return("Ocurrió un error al conectar a SQL Server: ", e)
